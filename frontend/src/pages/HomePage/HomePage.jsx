87fbb359c023998dce0981423afd0eed4cd07504
gerardosdl <gerardo.sdl.os@gmail.com>
2025-06-18 14:31:57 -0400

    Small edit in home page disclaimer

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -14,14 +14,14 @@ export default function HomePage() {
         }}
       />
       <p className="disclaimer">
-        <strong>Disclaimer: </strong>This app is designed to help users pick and
-        take notes on their strategy or observations over time. It uses the free
-        version of the Polygon API, which is limited to 5 calls per minute.
-        Please note that stock prices - including the "current price" and "price
-        when added" - are based on the previous day's closing data and may not
-        reflect real-time market conditions. This tool is for informational and
-        personal tracking purposes only, and should not be used as financial
-        advice.
+        <strong>Disclaimer: </strong>This app is designed to help users pick a
+        stock and take notes on their strategy or observations over time. It
+        uses the free version of the Polygon API, which is limited to 5 calls
+        per minute. Please note that stock prices - including the "current
+        price" and "price when added" - are based on the previous day's closing
+        data and may not reflect real-time market conditions. This tool is for
+        informational and personal tracking purposes only, and should not be
+        used as financial advice.
       </p>
     </>
   );
